7a3e0c5acc2733552d5843c84d0591d072d93b66
Denis Makhortov <z2.cooleo@gmail.com>
2017-12-29 14:09:13 +0300

    no message

---
+++ sendMail/Config.cs
@@ -0,0 +1,69 @@
+﻿using System.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace sendMail
+{
+    static class Config
+    {
+        public static string UserSMTP = "admin@vial.by";
+        public static string PassSMTP = "vial2016";
+        public static string fromSMTPmail = "admin@vial.by";
+        public static string toSMTPmail = "";
+        public static string serverSMTP = "mail.vial.by";
+        public static bool isExistXMLConfig()
+        {
+            return File.Exists("Config.xml") ? true : false;
+        }
+        public static void SaveConfig()
+        {
+            using (XmlWriter wr = XmlWriter.Create("config.xml"))
+            {
+                wr.WriteStartDocument();
+                wr.WriteStartElement("Settings");
+
+                wr.WriteStartElement("UserSMTP");
+                wr.WriteString(UserSMTP);
+                wr.WriteEndElement();
+
+                wr.WriteStartElement("PassSMTP");
+                wr.WriteString(PassSMTP);
+                wr.WriteEndElement();
+
+                wr.WriteStartElement("serverSMTP");
+                wr.WriteString(serverSMTP);
+                wr.WriteEndElement();
+
+                wr.WriteStartElement("toSMTPmail");
+                wr.WriteString(toSMTPmail);
+                wr.WriteEndElement();
+
+                wr.WriteStartElement("fromSMTPmail");
+                wr.WriteString(fromSMTPmail);
+
+                wr.WriteEndElement();
+                wr.WriteEndDocument();
+                wr.Close();
+            }
+        }
+        public static void ReadConfig()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("config.xml");
+            XmlNodeList elem = doc.GetElementsByTagName("UserSMTP");
+            UserSMTP = elem[0].InnerText;
+            elem = doc.GetElementsByTagName("PassSMTP");
+            PassSMTP = elem[0].InnerText;
+            elem = doc.GetElementsByTagName("serverSMTP");
+            serverSMTP = elem[0].InnerText;
+            elem = doc.GetElementsByTagName("toSMTPmail");
+            toSMTPmail = elem[0].InnerText;
+            elem = doc.GetElementsByTagName("fromSMTPmail");
+            fromSMTPmail = elem[0].InnerText;
+            doc = null;
+        }
+    }
+}
--- sendMail/MainWindow.xaml.cs
+++ sendMail/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
+using System.Net.Mail;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Net;
 
 namespace sendMail
 {
@@ -20,9 +13,90 @@ namespace sendMail
     /// </summary>
     public partial class MainWindow : Window
     {
+        BackgroundWorker bgWorker = new BackgroundWorker();
+        private int count;
+        private string mailSender;
+        private string message;
+    
         public MainWindow()
         {
             InitializeComponent();
+
+            if (!Config.isExistXMLConfig()) Config.SaveConfig();
+            else Config.ReadConfig();
+
+            mailTo.Text = Config.toSMTPmail;
+
+            bgWorker.WorkerReportsProgress = true;
+            bgWorker.DoWork += new DoWorkEventHandler(bgWorker_DoWork);
+            bgWorker.ProgressChanged += new ProgressChangedEventHandler(bgWorker_ProgressChanged);
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            btn.Content = "Stop";
+            Config.toSMTPmail = mailTo.Text;
+            Config.SaveConfig();
+
+            Label.Content = "";
+            int j = 0;
+            try
+            {
+                j = int.Parse(countMail.Text);
+            }
+            catch
+            {
+                countMail.Text = "1 -" + Int32.MaxValue;
+            }
+
+                if (mailTo.Text != "To:" || mailTo.Text != "")
+                {
+                count = int.Parse(countMail.Text);
+                progressBar.Maximum = count;
+                progressBar.Minimum = 0;
+                mailSender = mailTo.Text;
+                message = Message.Text;
+                bgWorker.RunWorkerAsync();
+                }
+        }
+
+        void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+                for (int i = count; i > 0; i--)
+                {
+                    SmtpClient client = new SmtpClient();
+                    client.Host = Config.serverSMTP;
+                    client.Port = 25;
+                    client.UseDefaultCredentials = false;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.EnableSsl = false;
+                    client.Credentials = new NetworkCredential(Config.UserSMTP, Config.PassSMTP);
+                    MailMessage msg = new MailMessage(Config.fromSMTPmail, Config.toSMTPmail, "Test", message);
+                    client.Send(msg);
+                    bgWorker.ReportProgress(i - 1);
+                }
+        }
+        void bgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // The progress percentage is a property of e          
+            progressBar.Value = e.ProgressPercentage;
+            countMail.Text = e.ProgressPercentage.ToString();
+            btn.IsEnabled = true;
+            if (e.ProgressPercentage == 0) btn.Content = "Send";
+        }
+        private void mailTo_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (mailTo.Text == "To:") mailTo.Text = "";          
+        }
+
+        private void Message_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Message.Text == "Msg:") Message.Text = "";
+        }
+
+        private void countMail_GotFocus(object sender, RoutedEventArgs e)
+        {
+            countMail.Text = "";
         }
     }
 }
